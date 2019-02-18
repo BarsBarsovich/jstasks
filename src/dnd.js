ecf6a73f75e660359ce000fc6434d307a71b622a
barspredator <boris.dryabin@gmail.com>
2019-02-18 21:28:52 +0300

    dnd started

--- src/dnd.js
+++ src/dnd.js
@@ -15,7 +15,8 @@
    const newDiv = document.createElement('div');
    homeworkContainer.appendChild(newDiv);
  */
-const homeworkContainer = document.querySelector('#homework-container');
+// window.onload = function () {
+const homeworkContainer = document.querySelector('.homework-container');
 
 /*
  Функция должна создавать и возвращать новый div с классом draggable-div и случайными размерами/цветом/позицией
@@ -26,7 +27,44 @@ const homeworkContainer = document.querySelector('#homework-container');
    const newDiv = createDiv();
    homeworkContainer.appendChild(newDiv);
  */
+
+const div = createDiv();
+
+homeworkContainer.appendChild(div);
+
 function createDiv() {
+
+    const element = document.createElement('div');
+
+    element.style.height = Math.floor(generateHeight()) + 'px';
+    element.style.width = Math.floor(generateWidth()) + 'px';
+    element.style.background = 'red';
+    element.setAttribute('id', 'addDiv');
+    element.setAttribute('draggable', true);
+
+    element.style.position= 'absolute';
+    element.style.top = Math.floor(generateTop()) +  'px';
+    element.style.left = Math.floor(generateLeft()) +  'px';
+    element.style.cursor = 'move';
+
+    function generateHeight() {
+        return Math.random() * 100;
+    }
+
+    function generateWidth() {
+        return Math.random() * 100;
+    }
+
+    function generateTop(){
+        return Math.random() * 200;
+    }
+
+
+    function generateLeft(){
+        return Math.random() * 150;
+    }
+
+    return element;
 }
 
 /*
@@ -37,12 +75,12 @@ function createDiv() {
    homeworkContainer.appendChild(newDiv);
    addListeners(newDiv);
  */
-function addListeners(target) {
-}
+// function addListeners(target) {
+// }
 
 let addDivButton = homeworkContainer.querySelector('#addDiv');
 
-addDivButton.addEventListener('click', function() {
+addDivButton.addEventListener('click', function () {
     // создать новый div
     const div = createDiv();
 
@@ -54,6 +92,3 @@ addDivButton.addEventListener('click', function() {
     // или использовать HTML5 D&D - https://www.html5rocks.com/ru/tutorials/dnd/basics/
 });
 
-export {
-    createDiv
-};
