405a826156e8ef8513034524e88427f0454f8876
barspredator <boris.dryabin@gmail.com>
2019-02-21 16:42:52 +0300

    dz5 finished

--- src/dnd.js
+++ src/dnd.js
@@ -17,6 +17,8 @@
  */
 const homeworkContainer = document.querySelector('#homework-container');
 
+homeworkContainer.appendChild(createDiv());
+
 /*
  Функция должна создавать и возвращать новый div с классом draggable-div и случайными размерами/цветом/позицией
  Функция должна только создавать элемент и задвать ему случайные размер/позицию/цвет
@@ -27,6 +29,38 @@ const homeworkContainer = document.querySelector('#homework-container');
    homeworkContainer.appendChild(newDiv);
  */
 function createDiv() {
+    const newDiv = document.createElement('div');
+
+    newDiv.setAttribute('draggable', 'true');
+    newDiv.setAttribute('id', 'addDiv');
+
+    newDiv.style.position = 'absolute';
+    newDiv.style.top = Math.floor(randomNumber()) + 'px';
+    newDiv.style.left = Math.floor(randomNumber()) + 'px';
+    newDiv.style.height = Math.floor(randomNumber()) + 'px';
+    newDiv.style.width = Math.floor(randomNumber()) + 'px';
+    newDiv.style.backgroundColor = randomColor();
+    newDiv.style.zIndex = 100;
+    newDiv.classList.add('draggable-div');
+
+    function randomNumber() {
+        return Math.random() * 100;
+    }
+
+    function randomColor() {
+        let letters = '0123456789ABCDEF';
+        let color = '#';
+
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+
+        return color;
+    }
+
+    addListeners(newDiv);
+
+    return newDiv;
 }
 
 /*
@@ -38,11 +72,21 @@ function createDiv() {
    addListeners(newDiv);
  */
 function addListeners(target) {
+    target.addEventListener('dragstart', function (event) {
+        event.target.style.opacity = 0.3;
+    })
+
+    target.addEventListener('dragend', function (event) {
+        event.stopPropagation();
+
+        event.target.style.top = event.screenX + 'px';
+        event.target.style.left = event.screenY + 'px';
+    });
 }
 
 let addDivButton = homeworkContainer.querySelector('#addDiv');
 
-addDivButton.addEventListener('click', function() {
+addDivButton.addEventListener('click', function () {
     // создать новый div
     const div = createDiv();
 
